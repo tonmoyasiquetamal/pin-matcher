2184265f6926fe4733d5ca86a16f79cd2d4b0d44
tonmoyasiquetamal <tonmoyasiquetamal.cse@gmail.com>
2021-08-20 01:56:40 +0600

    clearable digit

--- js/script.js
+++ js/script.js
@@ -1,25 +1,34 @@
 /* got pin  */
-function getPin(){
-    const pin = Math.round (Math.random()*10000);
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
     const pinString = pin + ''
-    if (pinString.length ==4){
+    if (pinString.length == 4) {
         return pin;
     }
-    else{
+    else {
         return getPin();
     }
 }
 /* generate pin in form */
-function generatePin (){
-   const pin = getPin();
-   document.getElementById('display-pin').value = pin;
+function generatePin() {
+    const pin = getPin();
+    document.getElementById('display-pin').value = pin;
 }
 /* event bubble using for calculator body */
-document.getElementById('calculator-keypad').addEventListener('click', function(event){
-const number = event.target.innerText;
-const calcInput = document.getElementById('typed-numbers');
-
-const previousNumber = calcInput.value;
-const newNumber = previousNumber + number;
-calcInput.value= newNumber;
+document.getElementById('calculator-keypad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+    /* not a number checking  */
+    if (isNaN(number)) {
+        /* clear numbers of value */
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+    }
+    else {
+        /* pin matcher input every digit number */
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
 });
